d1141ade14c90fb73b18b74e5624a1571f77a24a
wjlike@sina.cn <wjlike0125>
2021-05-22 15:24:54 +0800

    add

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-# Getting Started
+#### Flink	
 
-### Reference Documentation
-For further reference, please consider the following sections:
+```shell
+nc -lk  7777  #启动发送socket 文本流服务器  nc linux自带的一个命令 l 表示监听，监听某一端口 k 表示keep 保持当前的链接
+```
 
-* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
-* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.4.5/maven-plugin/reference/html/)
-* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.4.5/maven-plugin/reference/html/#build-image)
+
+
+
+
+## Flinl 部署
+
+ #### Standalone模式
+
+安装：
+
+解压缩。flink-1.10.1-bin-scala_2.12.tgz 进入conf目录中。
+
+​	1、修改 flink/conf/flink-conf.yaml 文件
+
+
+
+
+
+
 
--- flink/src/main/java/com/like/study/flink/wc/StreamWordCount.java
+++ flink/src/main/java/com/like/study/flink/wc/StreamWordCount.java
@@ -1,20 +1,35 @@
 package com.like.study.flink.wc;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class StreamWordCount {
+/**
+ * 流处理文件  无界流
+ */
+public class  StreamWordCount {
     public static void main(String[] args) throws Exception {
         //创建流处理执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+        //设置并行处理数据线程
+        env.setParallelism(1);
 
-        String filePath = "E:\\hello.txt";
+//        String filePath = "/Users/like/ideaPrjoect/my/study/MyStudy/flink/src/main/resources/hello.txt";
+//        //从文件中读取数据
+//        DataStream<String> dataStream = env.readTextFile(filePath);
 
-        //从文件中读取数据
-        DataStream<String> dataStream = env.readTextFile(filePath);
+        //从parameter tool 工具从程序启动参数中提取配置项
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+//        String host = parameterTool.get("host");
+//        int port = parameterTool.getInt("port");
+
+
+        //从socket 文本流读取数据
+        DataStream<String> inputStream = env.socketTextStream("localhost",7777);
         //基于数据流进行计算
-        DataStream<Tuple2<String, Integer>> dataStream1 = dataStream.flatMap(new WordCount.MyflatMapper())
+        DataStream<Tuple2<String, Integer>> dataStream1 = inputStream.flatMap(new WordCount.MyflatMapper())
                 .keyBy(0)
                 .sum(1);
         dataStream1.print();
