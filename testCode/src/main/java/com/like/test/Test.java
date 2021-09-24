7d97725f023db2b5b9fa5bdaa69bcda96633d20d
wjlike <625108339@qq.com>
2021-09-24 15:02:07 +0800

    +

---
+++ testCode/src/main/java/com/like/test/Test.java
@@ -0,0 +1,16 @@
+package com.like.test;
+
+import java.util.ArrayList;
+
+/**
+ * @Description: 写点啥
+ * @author: like
+ * @createTime: 2021/9/20 18:05
+ **/
+public class Test {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList(5);
+        list.add("s");
+
+    }
+}
